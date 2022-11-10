d9217aa062bc63ff4803a128f7975bae8fb79bd6
Emerson Miguel <emersongne@gmail.com>
2022-11-10 16:05:13 +0000

    Terminando o módulo de coleções.

--- Colecoes/ColecoesDictionary.cs
+++ Colecoes/ColecoesDictionary.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CursoCSharp.ClassesEMetodos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,7 +22,45 @@ namespace CursoCSharp.Colecoes
             //Vai verificar se tem o valor na coluna das chaves, seja, a primeira coluna.
             if (filmes.ContainsKey(2004))
             {
+                Console.WriteLine("2004 : " + filmes[2004]); //pegar o valor do filme usando a chave.
+                //Se tentar acessar uma chave que não existe pode dar erro, por isso há outra forma de pegar o valor
+                Console.WriteLine("2004 : " + filmes.GetValueOrDefault(2004)); //ele vai retornar, ou o valor da chave que colocou, ou o valor default que é uma string vazia.
 
+                Console.WriteLine(filmes.ContainsValue("Pulp Fiction")); //Função para saber se existe o valor nas chaves, retorna true ou false.
+                Console.WriteLine("Removeu? " + filmes.Remove(2004)); //função de remover vai retornar true ou false
+
+                //outra forma te tentar obter o valor é com TryGetValue e retorna o valor para uma variavel por referencia.
+                filmes.TryGetValue(2006, out string filme2006);
+                Console.WriteLine($"Filme  {filme2006}!");
+                //se não existe, ele nao vai retornar nada.
+                filmes.TryGetValue(2007, out string filme2007);
+                Console.WriteLine($"Filme  {filme2007}!");
+
+                //Come percorrer o dicionário (4 formas)
+
+                // 1 - Para cada uma das chaves, ele vai mostrar
+                foreach(var chave in filmes.Keys)
+                {
+                    Console.WriteLine(chave);
+                }
+
+                // 2 - Percorrendo somente os valores
+                foreach(var valor in filmes.Values) 
+                { 
+                    Console.WriteLine(valor);
+                } 
+
+                // 3 - KeyValuePair para apanhar os pares.
+                foreach(KeyValuePair<int, string> filme in filmes)
+                {
+                    Console.WriteLine($"{filme.Value} é de {filme.Key}.");
+                }
+
+                // 4 - A forma mais facil de achar o par é deixar o compilador inferir que essa variável é o par.
+                foreach(var filme in filmes) //aqui o compilador já vai inferir que esse filme é do tipo KeyValuePair
+                {
+                    Console.WriteLine($"{filme.Value} é de {filme.Key}.");
+                }
             }
         }
     }
